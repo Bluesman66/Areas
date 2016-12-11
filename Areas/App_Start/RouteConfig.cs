be76927494784848f17b925598f5b44a3544af2b
alex.paclin <alex.paclin@gmail.com>
2016-12-11 21:45:32 +0500

    Создание собственного обработчика маршрутов

--- Areas/App_Start/RouteConfig.cs
+++ Areas/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Areas.RouteHandlers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,13 +14,18 @@ namespace Areas
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-				namespaces: new[] { "Areas.Controllers" }
-				//namespaces: new[] { "Areas.Areas.Store.Controllers" }
-			);
+			//routes.MapRoute(
+			//	name: "Default",
+			//	url: "{controller}/{action}/{id}",
+			//	defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+			//	namespaces: new[] { "Areas.Controllers" }
+			//	//namespaces: new[] { "Areas.Areas.Store.Controllers" }
+			//);
+			
+			Route newRoute = new Route("{controller}/{action}", 
+				new RouteValueDictionary(new { controller = "Home", action = "Index", id = UrlParameter.Optional }),  
+				new MyRouteHandler());
+			routes.Add(newRoute);
 		}
 	}
 }
