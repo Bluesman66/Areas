81103a26b615a6eaebb92b9fc3c1733fc3d9aef8
alex.paclin <alex.paclin@gmail.com>
2016-12-11 21:21:45 +0500

    area Store added

--- Areas/App_Start/RouteConfig.cs
+++ Areas/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace Areas
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+				namespaces: new[] { "Areas.Controllers" }
+				//namespaces: new[] { "Areas.Areas.Store.Controllers" }
 			);
 		}
 	}
